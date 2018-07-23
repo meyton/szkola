47550dd1c3aa99293fad6f96c6b2e658a1895c8c
Marcin Wesel <marcin.wesel@pl.abb.com>
2018-07-23 17:32:04 +0200

    First Commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            lblHello.Text = "WSEI Szkoła programowania welcome to";
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(entryAge.Text))
+            {
+                await DisplayAlert("Błąd", "Musisz wpisać wiek", "OK");
+                return;
+            }
+
+            var age = entryAge.Text;
+
+            bool result = await DisplayAlert($"Masz {age} lat", "Czy masz ukończone 18 lat, by przejść na stronę?", "TAK", "NIE");
+            if (result)
+            {
+                await Navigation.PushAsync(new SecondPage());
+            }
+            else
+            {
+                await DisplayAlert("OK", "Nie przechodzimy na stronę", "OK");
+            }
+        }
+    }
+}
